da45df1163af0f608e003bc3f383b700f9bcb123
Jhonatan Teixeira Rios <jhonatan.rios@the9level.com>
2020-12-02 21:50:36 -0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd7a488fcab73efd12cebb473fde96491'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '883f5647-c5de-4017-82c5-c3eabf8c4342'
+};
